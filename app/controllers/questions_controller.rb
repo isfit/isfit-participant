556ef7534e08af43a27f2331a3d18073b9ec1e9c
StianF <stian.fredrikstad@gmail.com>
2010-09-30 17:59:06 +0200

    Added restrictions to questions and answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,19 @@
 class QuestionsController < ApplicationController
+  before_filter :authenticate_user!
+
   # GET /questions
   # GET /questions.xml
   def index
-    @questions = Question.all
+
+    if !user_signed_in?
+      redirect_to root_path
+    else
+    @questions = Question.find(:all, :conditions=>{:participant_id=>current_user.id})
 
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @questions }
+    end
     end
   end
 
@@ -42,7 +49,7 @@ class QuestionsController < ApplicationController
   # POST /questions.xml
   def create
     @question = Question.new(params[:question])
-
+    @question.participant_id = current_user.id
     respond_to do |format|
       if @question.save
         format.html { redirect_to(@question, :notice => 'Question was successfully created.') }
