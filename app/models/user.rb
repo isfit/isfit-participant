2e02e52c25839615856afe9be7d421b96a45f51c
dagingaa <daginge@gmail.com>
2010-10-04 17:58:24 +0200

    Quickfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   # roles
-  acts_as_authorization_subject  :association_name => :roles
+  #acts_as_authorization_subject  :association_name => :roles
   
 end
