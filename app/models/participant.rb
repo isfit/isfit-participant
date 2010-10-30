ebe796d48b2d9ca485a6a238db3b20ac9ce50bf5
IT <it@isfit.org>
2010-10-30 13:09:37 +0200

    Empty commit

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,6 +4,6 @@ class Participant < ActiveRecord::Base
   belongs_to :arrival_place
 
   #validations
-  validates_presence_of :first_name, :last_name, :email, :date_of_birth, :address1, :zipcode, :city, :field_of_study
-  validates_uniqueness_of :user_id
+#  validates_presence_of :first_name, :last_name, :email, :date_of_birth, :address1, :zipcode, :city, :field_of_study
+ # validates_uniqueness_of :user_id
 end
