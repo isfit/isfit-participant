329d6b2fbadb40c82faeefdb5a6314059c742ea2
Stian Fredrikstad <stianfr@isfit.org>
2010-11-17 18:08:06 +0100

    Removed functionary_id from participant

--- db/migrate/20101116003240_create_participants_functionaries.rb
+++ db/migrate/20101116003240_create_participants_functionaries.rb
@@ -2,7 +2,6 @@ class CreateParticipantsFunctionaries < ActiveRecord::Migration
   def self.up
     create_table :functionaries_participants, :id => false do |t|
       t.integer :participant_id
-      t.integer :functionary_id
       t.timestamps
     end
   end
