ca51b6b0f4c1475b225468c364b20c119c531441
Dag-Inge Aas <daginge@gmail.com>
2012-09-20 11:11:14 +0200

    Clean up scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//= require jquery_ujs
+//= require_tree .
+//= require_self
 $(document).ready( function() {
   $('.dropdown-toggle').dropdown();
 });
