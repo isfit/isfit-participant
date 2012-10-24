98d6b5f643daaeaaf18ad0c70d36fb25e60d19f5
krisvage <krisvaage@gmail.com>
2012-10-24 22:07:46 +0200

    Further bootstrapification of questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,32 +6,44 @@ class QuestionsController < ApplicationController
   load_and_authorize_resource
 
   def q_status
-    index  
+    index
   end
 
   # GET /questions
   # GET /questions.xml
   def index
-    selected_status = params[:status]
+    selected_status = params[:question][:status] if params[:question]
     status_query = ""
-    @status = Question.status_new
+    @status = selected_status || Question.status_new
     @statuses = Question.status_options
-    if selected_status != nil
-      status_query = "status = " + selected_status
-      @status = selected_status
-    else
-      status_query = "status = " + Question.status_new
-    end
+
+    status_query = "status = " + @status.to_s
+
     if current_user.has_role?(:admin)
-      @questions = Question.joins(:participant).all(:joins=>"JOIN functionaries_participants fp ON fp.participant_id = participants.id", :conditions=>status_query, :order=>"fp.functionary_id, questions.created_at DESC").paginate(:per_page => 10, :page=>params[:page])
-      #@questions = Question.all.paginate(:per_page => 10, :page => params[:page])
-      @static = Functionary.find_by_sql("SELECT f.first_name as first_name, f.last_name as last_name, count(CASE WHEN q.status = 1 THEN f.id END) as new, count(CASE WHEN q.status = 2 THEN f.id END) as opened, count(CASE WHEN q.status = 3 THEN f.id END) as resolved FROM functionaries f JOIN functionaries_participants fp ON fp.functionary_id = f.id JOIN participants p ON p.id = fp.participant_id JOIN questions q ON q.participant_id = p.id GROUP BY f.id");
+      @questions = Question
+        .where(status_query)
+        .order("questions.created_at DESC")
+        .paginate(:per_page => 10, :page=>params[:page])
+
+      @static = Functionary.find_by_sql('SELECT f.first_name as first_name, f.last_name as last_name'\
+        ', count(CASE WHEN q.status = 1 THEN f.id END) as new, count(CASE WHEN q.status = 2 THEN f.id END)'\
+        ' as opened, count(CASE WHEN q.status = 3 THEN f.id END) as resolved FROM functionaries f'\
+        ' JOIN functionaries_participants fp ON fp.functionary_id = f.id JOIN participants p ON p.id'\
+        '= fp.participant_id JOIN questions q ON q.participant_id = p.id GROUP BY f.id');
       render :index_nopart
     elsif current_user.has_role?(:dialogue)
-      @questions = Question.all(:conditions=>"dialogue = 1"+status_query, :order=>"questions.created_at DESC").paginate(:per_page => 10, :page=>params[:page])
+      @questions = Question
+        .all(:conditions=>"dialogue = 1"+status_query, :order=>"questions.created_at DESC")
+        .paginate(:per_page => 10, :page=>params[:page])
+
       render :index_nopart
     elsif current_user.has_role?(:functionary)
-      @questions = Question.joins(:participant).all(:joins=>"JOIN functionaries_participants fp ON fp.participant_id = participants.id", :conditions=>statusq+" AND fp.functionary_id = "+current_user.functionary.id.to_s, :order=>"questions.created_at DESC").paginate(:per_page => 10, :page => params[:page])
+      @questions = Question
+        .joins(:participant).all(:joins=>"JOIN functionaries_participants fp ON fp.participant_id = participants.id",
+          :conditions=>statusq+" AND fp.functionary_id = "+current_user.functionary.id.to_s,
+          :order=>"questions.created_at DESC")
+        .paginate(:per_page => 10, :page => params[:page])
+
       render :index_nopart
     else
       @questions = Question.find(:all, :conditions=>"participant_id="+current_user.participant.id.to_s)
@@ -79,9 +91,7 @@ class QuestionsController < ApplicationController
   def edit
     @question = Question.find(params[:id])
     @statuses = Question.status_options
-    if !current_user.is_participant? || @question.participant.user == current_user
-      
-    else
+    if current_user.is_participant? || @question.participant.user != current_user
       raise CanCan::AccessDenied
     end 
   end
