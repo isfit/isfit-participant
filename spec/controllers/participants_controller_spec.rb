88488b8fb0033606cd43c47c22aa0729630af8ba
Kristian Våge <krisvaage@gmail.com>
2013-01-10 12:36:01 +0100

    participants#index spec passing

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -5,15 +5,16 @@ describe ParticipantsController do
 		fixtures :users, :roles, :user_roles, :participants
 
 		before(:each) do
-			user = users(:one)
-			controller.stub(current_user: user)
+			@user = users(:one)
+			sign_in @user
+			controller.stub(current_user: @user)
 		end
 
 		it "GET index" do
-			participant = stub_model(Participant)
-			Participant.stub(:all) { [participant] }
+			#participant = stub_model(Participant)
+			#Participant.stub(:all) { [participant] }
 			get :index
-			expect(assigns(:participants)).to eq([participant])
+			expect(assigns(:participants)).to eq([participants(:two), participants(:one)])
 		end
 	end
 end
