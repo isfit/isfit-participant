b3b2e8c0a73369b9e3add49589f886aa750b057c
Kristian Våge <krisvaage@gmail.com>
2013-05-01 19:56:35 +0200

    Made travis script executable

--- config/ci/before_travis.sh
+++ config/ci/before_travis.sh
@@ -1,7 +1,9 @@
+echo "Setting up database.yml for test (mysql)"
 cp config/database.yml.example config/database.yml
 
-mysql -e 'CREATE database isfit13_pw_test;'
+mysql -e 'create database isfit13_pw_test;'
 
+echo "Set up test database"
 bundle exec rake db:create
 bundle exec rake db:schema:load
 
