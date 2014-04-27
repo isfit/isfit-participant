e1193c9cc88f3ba56c0c811e6f0a697d6437d29e
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-04-27 20:44:14 +0200

    Gives new users an applicant role

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    @user = User.new(params[:user])
+    applicant_role = Role.where(name: 'applicant')
+
+    if @user.save
+      @user.roles << applicant_role
+      sign_in @user
+      redirect_to dashboard_url, :notice => 'Your profile was successfully created. We can\'t wait for your application!'
+    else
+      params[:user][:password] = nil
+      params[:user][:password_confirmation] = nil
+      render action: 'new'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 #Create roles
-admin = Role.create(:name => "admin")
+Role.create(:name => "admin")
 Role.create(:name => "theme")
 Role.create(:name => "functionary")
 Role.create(:name => "sec")
@@ -20,24 +20,17 @@ TransportType.create(:id => 8,:name => "Car")
 #Create control panel entry
 ControlPanel.create(:app_grade1 => 0, :app_grade2 => 0, :app_grade3 => 0, :app_grade2_scope => 0)
 
-#Create first user
-User.create!(:email => "root@isfit.org", :password => "sdfiky7f23ohfasldjfbd923hd29bhsidywqdhwejdbuiwhdo8ncskdncjkshfi2o3")
-User.last.roles << admin
-f = Functionary.create!(:first_name => "Placeholder", :last_name => "Placeholdersen")
-f.user = User.last
-f.save
-
 #Deadlines
-Deadline.create(:name => "1. Accept invitation", :deadline => "2014-11-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "2. Apply for visa", :deadline => "2014-11-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "3. Embassy confirmation", :deadline => "2014-11-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "4. Valid passport", :deadline => "2014-11-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "5. Update profile", :deadline => "2014-11-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "6. Granted vias", :deadline => "2014-12-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "7. Need transport", :deadline => "2015-01-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "8. Flight tickets", :deadline => "2015-01-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "9. Confirm participation", :deadline => "2015-01-15 23:59", :participant_type: => 1)
-Deadline.create(:name => "1. Accept waiting-list", :deadline => "2014-11-15 23:59", :participant_type: => 2)
+Deadline.create(:name => "1. Accept invitation", :deadline => "2014-11-15 23:59", :participant_type => 1)
+Deadline.create(:name => "2. Apply for visa", :deadline => "2014-11-15 23:59", :participant_type => 1)
+Deadline.create(:name => "3. Embassy confirmation", :deadline => "2014-11-15 23:59", :participant_type => 1)
+Deadline.create(:name => "4. Valid passport", :deadline => "2014-11-15 23:59", :participant_type => 1)
+Deadline.create(:name => "5. Update profile", :deadline => "2014-11-15 23:59", :participant_type => 1)
+Deadline.create(:name => "6. Granted vias", :deadline => "2014-12-15 23:59", :participant_type => 1)
+Deadline.create(:name => "7. Need transport", :deadline => "2015-01-15 23:59", :participant_type => 1)
+Deadline.create(:name => "8. Flight tickets", :deadline => "2015-01-15 23:59", :participant_type => 1)
+Deadline.create(:name => "9. Confirm participation", :deadline => "2015-01-15 23:59", :participant_type => 1)
+Deadline.create(:name => "1. Accept waiting-list", :deadline => "2014-11-15 23:59", :participant_type => 2)
 
 #Countries???
 
