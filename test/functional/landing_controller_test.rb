8735237253ab58fc90c29f7d3a4378e5de9b20c7
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-05-09 01:00:47 +0200

    Adds tests for the landing controller

--- test/functional/landing_controller_test.rb
+++ test/functional/landing_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class LandingControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index when logged out' do
+    get :index
+    assert_response :success
+  end
+
+  test 'should get redirect when logged in' do
+    sign_in users(:kyle)
+
+    get :index
+    assert_redirected_to dashboard_url
+  end
 end
