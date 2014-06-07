324f238ae5799d4a1e0762928ec05439dcdf703f
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-06-07 20:35:34 +0200

    Open users to send in dialogue applications

--- app/controllers/dialogue_applications_controller.rb
+++ app/controllers/dialogue_applications_controller.rb
@@ -1,6 +1,4 @@
 class DialogueApplicationsController < ApplicationController
-  # GET /dialogue_applications/1
-  # GET /dialogue_applications/1.json
   def show
     @dialogue_application = DialogueApplication.find(params[:id])
 
@@ -10,15 +8,8 @@ class DialogueApplicationsController < ApplicationController
     end
   end
 
-  # GET /dialogue_applications/new
-  # GET /dialogue_applications/new.json
   def new
     @dialogue_application = DialogueApplication.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @dialogue_application }
-    end
   end
 
   # GET /dialogue_applications/1/edit
@@ -26,24 +17,17 @@ class DialogueApplicationsController < ApplicationController
     @dialogue_application = DialogueApplication.find(params[:id])
   end
 
-  # POST /dialogue_applications
-  # POST /dialogue_applications.json
   def create
     @dialogue_application = DialogueApplication.new(params[:dialogue_application])
+    @dialogue_application.user_id = params[:user_id]
 
-    respond_to do |format|
-      if @dialogue_application.save
-        format.html { redirect_to @dialogue_application, notice: 'Dialogue application was successfully created.' }
-        format.json { render json: @dialogue_application, status: :created, location: @dialogue_application }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @dialogue_application.errors, status: :unprocessable_entity }
-      end
+    if @dialogue_application.save
+      redirect_to dashboard, notice: 'Dialogue application was successfully saved. You can edit it until the 30th of september.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /dialogue_applications/1
-  # PUT /dialogue_applications/1.json
   def update
     @dialogue_application = DialogueApplication.find(params[:id])
 
@@ -58,8 +42,6 @@ class DialogueApplicationsController < ApplicationController
     end
   end
 
-  # DELETE /dialogue_applications/1
-  # DELETE /dialogue_applications/1.json
   def destroy
     @dialogue_application = DialogueApplication.find(params[:id])
     @dialogue_application.destroy
