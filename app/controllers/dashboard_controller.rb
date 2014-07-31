b9577d7b934b064a4a3ae4f15d112a63e690c211
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-07-31 10:35:47 +0200

    Adds basic statistics for admin dashboards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def index
     if current_user.has_role?(:admin)
+      @number_of_users = User.count
+      @number_of_profiles = Profile.count
+
       render template: 'dashboard/index_admin'
     elsif current_user.has_role?(:applicant)
       render template: 'dashboard/index_applicant'
