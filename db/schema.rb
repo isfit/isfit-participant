8456a2bf2d7f528da6af59227ad1e0bad25a0474
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-08-04 13:38:07 +0200

    Adds single role field to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140722085450) do
+ActiveRecord::Schema.define(:version => 20140804111244) do
 
   create_table "answers", :force => true do |t|
     t.datetime "created_at",  :null => false
@@ -173,8 +173,9 @@ ActiveRecord::Schema.define(:version => 20140722085450) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",   :null => false
-    t.string   "encrypted_password",     :default => "",   :null => false
+    t.string   "email",                  :default => "",          :null => false
+    t.string   "role",                   :default => "applicant"
+    t.string   "encrypted_password",     :default => "",          :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -183,8 +184,8 @@ ActiveRecord::Schema.define(:version => 20140722085450) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.datetime "created_at",                                      :null => false
+    t.datetime "updated_at",                                      :null => false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
