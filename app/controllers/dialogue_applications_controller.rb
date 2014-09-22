87c28e540d0c280956bda47476b60abfe8f271fd
Per Berg <peberg.91@gmail.com>
2014-09-22 22:05:23 +0200

    created show.html.erb not finished

--- app/controllers/dialogue_applications_controller.rb
+++ app/controllers/dialogue_applications_controller.rb
@@ -3,5 +3,20 @@ class DialogueApplicationsController < ApplicationController
   def index
 	 @dialogue_applications = DialogueApplication.all
   end
+
+  def new
+  	@dialogue_applications.new
+  end
+
+  def show
+  	@dialogue_application = DialogueApplication.find(params[:id])
+  end
+
+  def destroy
+  	@dialogue_applications.destroy
+  	redirect_to dialogue_applications_url
+  end
+
+
   
 end
