ae08da0e8abf5ca86af160ab0367b44c5f09bf2a
Kristian Våge <krisvaage@gmail.com>
2014-10-07 21:01:40 +0200

    If user does not immidiately grade applicant, get option to come back to profile

--- app/controllers/review/profiles_controller.rb
+++ app/controllers/review/profiles_controller.rb
@@ -1,8 +1,10 @@
 class Review::ProfilesController < ApplicationController
   load_and_authorize_resource :workshop_application
-  
+
   def index
     @applications = WorkshopApplication.paginate(page: params[:page]).joins(:user, :profile).where(users: {role: 'applicant'}).where("profiles.motivation_essay != ''").where("profile_grade IS NULL").order('users.first_name ASC', 'users.last_name ASC')
+    current_user_ungraded_applications = WorkshopApplication.ungraded_applications.where(profile_reviewer_id: current_user.id)
+    @ungraded_application = current_user_ungraded_applications.order('users.first_name ASC', 'users.last_name ASC').readonly(false).first
 
     @number_of_applications = WorkshopApplication.joins(:user, :profile).where(users: {role: 'applicant'}).where("profiles.motivation_essay != ''").count
     @number_of_reviewed_applications = WorkshopApplication.joins(:user, :profile).where(users: {role: 'applicant'}).where("profiles.motivation_essay != ''").where("profile_grade IS NOT NULL").count
@@ -19,12 +21,12 @@ class Review::ProfilesController < ApplicationController
     if @application.save
       redirect_to review_profiles_path, notice: 'Profile grade was successfully set.'
     else
-     render :show
+      render :show
     end
   end
 
   def fetch
-    @application = WorkshopApplication.joins(:user, :profile).where(users: {role: 'applicant'}).where("profiles.motivation_essay != ''").where("profile_grade IS NULL").where("profile_reviewer_id IS NULL").order('users.first_name ASC', 'users.last_name ASC').readonly(false).first
+    @application = WorkshopApplication.ungraded_applications.where("profile_reviewer_id IS NULL").order('users.first_name ASC', 'users.last_name ASC').readonly(false).first
 
     if @application.nil?
       redirect_to review_profiles_url, notice: 'No more applications to review.'
