71582d7a4172cf706a8ed0d6bdc2f5123e21e76b
Per Berg <peberg.91@gmail.com>
2014-10-15 20:14:58 +0200

    Fixes

--- app/controllers/workshop_applications_controller.rb
+++ app/controllers/workshop_applications_controller.rb
@@ -1,4 +1,9 @@
 class WorkshopApplicationsController < ApplicationController
+  load_and_authorize_resource
+  
+  def index
+    @workshop_applications = WorkshopApplication.paginate(page: params[:page])
+  end
 
 def index
 	@workshop_applications = WorkshopApplication.paginate(page: params[:page])
