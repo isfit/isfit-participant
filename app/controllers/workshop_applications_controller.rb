09ef1425379602310019fc9f140d928ab5653329
Per Berg <peberg.91@gmail.com>
2014-10-22 20:48:32 +0200

    Adds index view for workshop applications

--- app/controllers/workshop_applications_controller.rb
+++ app/controllers/workshop_applications_controller.rb
@@ -2,5 +2,8 @@ class WorkshopApplicationsController < ApplicationController
   load_and_authorize_resource
   
   def index
+    @applications = WorkshopApplication.valid.paginate(page: params[:page]).
+      order('users.first_name ASC', 'users.last_name ASC')
   end
 end
+
