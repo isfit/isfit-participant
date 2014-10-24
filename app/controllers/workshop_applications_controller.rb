657cb7059f436c31533a3cad2692d19a9c57b0ee
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-10-24 14:13:45 +0200

    Added search to workshop applications

--- app/controllers/workshop_applications_controller.rb
+++ app/controllers/workshop_applications_controller.rb
@@ -2,7 +2,15 @@ class WorkshopApplicationsController < ApplicationController
   load_and_authorize_resource
   
   def index
-    @applications = WorkshopApplication.valid.paginate(page: params[:page]).
+    @applications = WorkshopApplication.valid
+
+    if params[:name].present?
+      keyword = "%#{params[:name]}%"
+      @applications = @applications.
+        where('users.first_name LIKE ? OR users.last_name LIKE ?', keyword, keyword)
+    end
+
+    @applications = @applications.paginate(page: params[:page]).
       order('users.first_name ASC', 'users.last_name ASC')
   end
 end
