88eb516dea02db36dead192707881a3ebde15ccc
Adrian Alexander Eriksen <adrian@aeriksen.com>
2014-10-31 15:03:17 +0100

    Adds logic for accepting invitation

--- app/controllers/deadlines/invitation_controller.rb
+++ app/controllers/deadlines/invitation_controller.rb
@@ -3,6 +3,16 @@ class Deadlines::InvitationController < ApplicationController
   end
 
   def update
+    @participant = current_user.participant
+
+    if params[:commit] == 'Yes'
+      @participant.accepted_invitation = 1
+    else
+      @participant.accepted_invitation = 0
+    end
+
+    @participant.save
+    redirect_to dashboard_url
   end
 
   def download
