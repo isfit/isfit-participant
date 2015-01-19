aae45d8dd37ba2a46e186641e466acb2c3cf5aa5
Adrian Alexander Eriksen <adrian@aeriksen.com>
2015-01-19 21:21:57 +0100

    Fixes bug in flight number validator

--- app/validators/flight_designator_validator.rb
+++ app/validators/flight_designator_validator.rb
@@ -1,6 +1,6 @@
 class FlightDesignatorValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^[a-zA-Z]{2,3}[0-9]{1,4}$/
+    unless value =~ /^[0-9a-zA-Z]{2,3}[0-9]{1,4}$/
       record.errors[attribute] << (options[:message] || 'is not a valid flight designator')
     end
   end
