ceef5cf4850a828013149d585ca8599d179a1a18
Adrian Alexander Eriksen <adrian@aeriksen.com>
2015-01-26 20:12:44 +0100

    Removes old mailers again

--- app/mailers/applicant_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ApplicantMailer < ActionMailer::Base
-  default from: 'no-reply@isfit.org'
-
-  def waiting_list_rejection_mail(user)
-    mail(to: user.email, subject: '[ISFiT] Regarding your place on the waiting list')
-  end
-end
