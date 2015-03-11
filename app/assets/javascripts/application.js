93824035583befcbf61ce1f1f833a2cbac25be9d
Adrian Alexander Eriksen <adrian@aeriksen.com>
2015-03-11 20:46:28 +0100

    Cleans assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,4 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+//= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= require_tree .
-//= require_self
-$(document).ready( function() {
-  $('.dropdown-toggle').dropdown();
-});
-
