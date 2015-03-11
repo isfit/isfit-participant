293862162ca31955cf746ed71877b6098b758fed
Adrian Alexander Eriksen <adrian@aeriksen.com>
2015-03-11 21:02:18 +0100

    Clean start for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,28 +1,10 @@
-ENV["RAILS_ENV"] = "test"
-
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'capybara/rails'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-end
-
-class ActionController::TestCase
-	include Devise::TestHelpers
-end
-
-class ActionDispatch::IntegrationTest
-  include Capybara::DSL
-
-  def teardown
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
-  end
 end
